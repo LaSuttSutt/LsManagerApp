14b472257e6ad32bb164052a68a4ad230f1dba41
Sven Sutter <sven.sutter@sotax.com>
2024-11-27 12:13:18 +0100

    NavBtn with event

--- LsManagerDesktop/UI/__Shared/Controls/NavigationButton.axaml.cs
+++ LsManagerDesktop/UI/__Shared/Controls/NavigationButton.axaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Input;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Interactivity;
 using Avalonia.Media;
@@ -10,21 +11,31 @@ namespace LsManagerDesktop.UI.__Shared.Controls;
 
 public class NavigationButton : TemplatedControl
 {
-    public static readonly StyledProperty<Geometry> ButtonIconProperty = AvaloniaProperty.Register<NavigationButton, Geometry>(
-        nameof(ButtonIcon));
+    public static readonly StyledProperty<Geometry> ButtonIconProperty =
+        AvaloniaProperty.Register<NavigationButton, Geometry>(
+            nameof(ButtonIcon));
 
-    public static readonly RoutedEvent<RoutedEventArgs> ClickEvent =
+    private static readonly RoutedEvent<RoutedEventArgs> ClickEvent =
         RoutedEvent.Register<NavigationButton, RoutedEventArgs>(nameof(Click), RoutingStrategies.Bubble);
-    
+
+
     public Geometry ButtonIcon
     {
         get => GetValue(ButtonIconProperty);
         set => SetValue(ButtonIconProperty, value);
     }
-    
+
     public event EventHandler<RoutedEventArgs>? Click
     {
         add => AddHandler(ClickEvent, value);
         remove => RemoveHandler(ClickEvent, value);
+    }
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+
+        if (e.NameScope.Find("NavBtn") is Button btn)
+            btn.Click += (_, _) => RaiseEvent(new RoutedEventArgs(ClickEvent, this));
     }
 }
