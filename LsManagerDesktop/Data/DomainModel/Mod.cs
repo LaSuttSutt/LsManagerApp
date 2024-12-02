4d6d636843c35138f273e4e27feef35de3776eb0
Sven Sutter <sven.sutter@sotax.com>
2024-12-02 12:57:12 +0100

    DataModel

---
+++ LsManagerDesktop/Data/DomainModel/Mod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LsManagerDesktop.Data.DomainModel;
+
+public class Mod
+{
+    [Key] public Guid Id { get; set; } = Guid.NewGuid();
+    
+    [MaxLength(150)] public string Name { get; set; } = "New Mod";
+    
+    [MaxLength(150)] public string Category { get; set; } = "Others";
+    
+    [MaxLength(150)] public string SubCategory { get; set; } = "";
+    
+    public Version Version { get; set; } = new Version(1, 0, 0, 0);
+    
+    [MaxLength(50)]
+    public string ModId { get; set; } = "0";
+    
+    public bool IsOfficial { get; set; } = false;
+
+    public bool IsDefault { get; set; } = false;
+    
+    [MaxLength(200)] public string Author { get; set; } = "";
+    public string FileName { get; set; } = "";
+}
